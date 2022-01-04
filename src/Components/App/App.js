efc27143c1a44edfb1221962ee088b621ce056fc
Akira Brand <akira@fusionauth.io>
2022-01-04 11:18:12 -0700

    adds iframe and loginout component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Shops from '../Shops/Shops'
 import Form from '../Form/Form'
 import NavBar from '../NavBar/NavBar'
+import LogInOut from '../LogInOut/LogInOut'
 import './App.css'
 
 class App extends Component {
@@ -10,8 +11,9 @@ class App extends Component {
     this.state = {
       shops: [
         {id: 1, title: 'Biyaal Trading', description: 'Maize seeds, Navajo tea, and more', url: 'https://biyl-trading.square.site/'},
-        {id: 2, title: 'Cheekbone Beauty', description: 'Sustainable, highly-pigmented, cosmetics', url: 'https://www.cheekbonebeauty.com/'}
-      ]
+        {id: 2, title: 'Cheekbone Beauty', description: 'Sustainable, highly-pigmented cosmetics', url: 'https://www.cheekbonebeauty.com/'},
+      ],
+      isClicked: false
     }
   }
 
@@ -19,12 +21,31 @@ class App extends Component {
     this.setState({shops: [...this.state.shops, newShop]});
   }
 
+  handleClick = () => {
+  //when login clicked, render the LogInOut component
+  //change isClicked to true
+  //kick off the auth flow here
+  if(!this.state.isClicked){
+    this.setState({isClicked: true})
+  } else {
+    console.log("is this an easter egg?")
+    this.setState({isClicked:false})
+}
+}
+
   render() {
     return (
 
         <main className='app'>
           <h1>Native Brands</h1>
-          <NavBar/>
+          <NavBar handleClick={this.handleClick}
+                  isClicked={this.state.isClicked}
+                  />
+          {this.state.isClicked && !this.state.error &&
+          <>
+            <LogInOut/>
+          </>
+          }
           <Form addStore={this.addStore} />
           <Shops shops={this.state.shops} />
         </main>
---
+++ src/Components/LogInOut/LogInOut.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './LogInOut.css'
+const LogInOut = () => {
+
+  return (
+    <div className='iframe'>
+      <iframe id="inlineFrameExample"
+       title="Akira's Website"
+       src="http://www.akirabrand.com">
+      </iframe>
+    </div>
+  )
+}
+export default LogInOut;
